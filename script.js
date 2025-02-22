0aaeaa76313bb179e64a536df4ed1a37a7f963fb
R S R I D O Y <rsridoykhan11111@gmail.com>
2025-02-22 12:01:40 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "GAMER", "PHOTOGRAPHER "],
+var typed1 = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "GAMER", "PHOTOGRAPHER"],
     typeSpeed: 100,
     backSpeed: 100,
-    BackDelay: 1000,
+    backDelay: 1000,
     loop: true
-})
+});
 
-var typed = new Typed(".multiple-text2", {
+var typed2 = new Typed(".multiple-text2", {
     strings: ["PORTFOLIO...", "R S R I D O Y..."],
     typeSpeed: 100,
     backSpeed: 100,
-    BackDelay: 1000,
+    backDelay: 1000, 
     loop: true
-})
+});
+function toggleMenu() {
+    document.querySelector(".navbar").classList.toggle("show");
+}
+document.querySelectorAll(".navbar a").forEach(item => {
+    item.addEventListener("click", () => {
+        document.querySelector(".navbar").classList.remove("show");
+    });
+});
