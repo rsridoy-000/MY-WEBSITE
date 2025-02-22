7c6f0830078933178250936cef86e455aebbe185
R S R I D O Y <rsridoykhan11111@gmail.com>
2025-02-22 21:29:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ var typed2 = new Typed(".multiple-text2", {
     backDelay: 1000, 
     loop: true
 });
+var typed = new Typed(".multiple-text3", {
+    strings: ["My name is R S RIDOY. I'm currently a 5th-semester student of the Computer Science Department at Jessore Polytechnic Institute, and I'm the captain of my class. I aspire to become a skilled Web Apps Developer. I have gained proficiency in HTML, CSS, and JavaScript, and I'm actively working with these technologies."],
+    typeSpeed: 50,
+  }
+    )
 function toggleMenu() {
     document.querySelector(".navbar").classList.toggle("show");
 }
